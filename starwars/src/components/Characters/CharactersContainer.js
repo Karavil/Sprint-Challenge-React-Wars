5cfae0fc126c80e354721dd2685870dcf7e94c8f
Alex Karavil <alex@karavil.io>
2020-02-21 12:16:30 -0500

    Implemented an wait homeworld lookup inside character load using Promise.all

--- starwars/src/components/Characters/CharactersContainer.js
+++ starwars/src/components/Characters/CharactersContainer.js
@@ -1,49 +1,68 @@
-import React, {useState, useEffect} from 'react'
-import axios from 'axios';
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const CharactersContainer = () => {
-  const [characters, setCharacters] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [nextButton, setNextButton] = useState(true);
-  const [prevButton, setPrevButton] = useState(false);
+  const [characters, setCharacters] = useState([])
+  const [currentPage, setCurrentPage] = useState(1)
+  const [nextButton, setNextButton] = useState(true)
+  const [prevButton, setPrevButton] = useState(false)
 
-  const STARWARS_PEOPLE_API = `https://swapi.co/api/people/?page=${currentPage}`;
+  const STARWARS_PEOPLE_API = `https://swapi.co/api/people/?page=${currentPage}`
 
   useEffect(() => {
     async function fetchCharacterData() {
       try {
-        let res = await axios.get(STARWARS_PEOPLE_API);
-
-        setCharacters(res.data.results.map(character => {
+        let res = await axios.get(STARWARS_PEOPLE_API)
+        const loadNewCharacters = res.data.results.map(async character => {
           const newCharacter = {
-
+            name: character.name,
+            homeworld: await getCharacterWorldName(character.homeworld),
           }
-        }));
-
-      } catch (err) {
-        console.log(err);
+          return newCharacter
+        })
+        Promise.all(loadNewCharacters).then(newCharacters =>
+          setCharacters(newCharacters),
+        )
+      } catch (error) {
+        console.log(error)
       }
     }
 
-    updateButtonStatus();
-    fetchCharacterData();
+    updateButtonStatus()
+    fetchCharacterData()
   }, [currentPage])
 
+  const getCharacterWorldName = async worldApiURL => {
+    try {
+      let res = await axios.get(worldApiURL)
+      return res.data.name
+    } catch (error) {
+      console.log(error)
+      return undefined
+    }
+  }
+
   const nextPage = () => {
-    setCurrentPage(currentPage => currentPage + 1);
+    setCurrentPage(currentPage => currentPage + 1)
     if (currentPage === 9) {
-      setNextButton(false);
+      setNextButton(false)
     }
   }
 
   const updateButtonStatus = () => {
-    setPrevButton(currentPage !== 1);
+    setPrevButton(currentPage !== 1)
     setNextButton(currentPage !== 9)
   }
 
   return (
-    <div></div>
+    <div>
+      {characters.map(character => (
+        <p key={character.name}>
+          {character.name} {character.homeworld}
+        </p>
+      ))}
+    </div>
   )
 }
 
-export default CharactersContainer;
+export default CharactersContainer

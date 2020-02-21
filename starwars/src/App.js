53ca56ff8ffe240c8ae9cd14fd9ed5ac36ca6fdd
Alex Karavil <alex@karavil.io>
2020-02-21 11:24:45 -0500

    Done with initial setup of project. Converted everything from CSS to Styled Components

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,19 +1,47 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import styled, { createGlobalStyle } from 'styled-components'
+
+import CharactersContainer from './components/Characters/CharactersContainer';
+
+import backgroundImage from './sw-bg.jpg';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    height: 100vh;
+    margin: 0;
+    padding: 0;
+
+    text-align: center;
+
+    background-image: url(${backgroundImage});
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+
+    font-family: sans-serif;
+  }
+`
+
+const Header = styled.h1`
+  color: #443e3e;
+  text-shadow: 1px 1px 5px #fff;
+`
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
-  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
   return (
     <div className="App">
-      <h1 className="Header">React Wars</h1>
+      <GlobalStyle />
+      <Header>React Wars</Header>
+      <CharactersContainer/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ starwars/src/components/Characters/CharactersContainer.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react'
+
+const CharactersContainer = () => {
+  const [characters, setCharacters] = useState([]);
+
+  return (
+    <div></div>
+  )
+}
+
+export default CharactersContainer;

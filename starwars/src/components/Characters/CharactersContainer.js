92e6a3e928e591989dd7a026ad65f227a067815f
Alex Karavil <alex@karavil.io>
2020-02-21 11:50:13 -0500

    Working on API connection for Starwars characters

--- starwars/src/components/Characters/CharactersContainer.js
+++ starwars/src/components/Characters/CharactersContainer.js
@@ -1,7 +1,45 @@
 import React, {useState, useEffect} from 'react'
+import axios from 'axios';
 
 const CharactersContainer = () => {
   const [characters, setCharacters] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [nextButton, setNextButton] = useState(true);
+  const [prevButton, setPrevButton] = useState(false);
+
+  const STARWARS_PEOPLE_API = `https://swapi.co/api/people/?page=${currentPage}`;
+
+  useEffect(() => {
+    async function fetchCharacterData() {
+      try {
+        let res = await axios.get(STARWARS_PEOPLE_API);
+
+        setCharacters(res.data.results.map(character => {
+          const newCharacter = {
+
+          }
+        }));
+
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    updateButtonStatus();
+    fetchCharacterData();
+  }, [currentPage])
+
+  const nextPage = () => {
+    setCurrentPage(currentPage => currentPage + 1);
+    if (currentPage === 9) {
+      setNextButton(false);
+    }
+  }
+
+  const updateButtonStatus = () => {
+    setPrevButton(currentPage !== 1);
+    setNextButton(currentPage !== 9)
+  }
 
   return (
     <div></div>
